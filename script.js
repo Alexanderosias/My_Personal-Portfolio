c74be414d9881dbf12d8a59d399e439e8b544abb
Alexander <alexanderosias123@gmail.com>
2025-06-26 13:33:26 +0800

    Toggle nav links and icon on menu click

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function showLinks() {
-      document.getElementById("link").style = "display: inline-block"
-    }
+  document.getElementById("link").style = "display: block;";
+  document.getElementById("close").style = "display: block;";
+  document.getElementById("menu").style = "display: none;";
+}
+
+function hideLinks() {
+  document.getElementById("link").style = "display: none;";
+  document.getElementById("close").style = "display: none;";
+  document.getElementById("menu").style = "display: block;";
+}
